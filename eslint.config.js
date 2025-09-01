ca86ef63f61862b5cedf6e34486a8adaea063f50
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2025-09-01 20:06:38 +0200

    Feat[root]: modification rules new lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,75 +1,147 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier' // <= ADICIONE
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import importPlugin from 'eslint-plugin-import'
-import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist', 'build', 'coverage', 'node_modules'] },
+  {
+    ignores: ['dist', 'build', 'coverage', 'node_modules'],
+  },
 
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier, // mantém para desativar regras de formatação conflitantes
+    ],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     settings: {
-      react: { version: 'detect' },
-      'import/resolver': { typescript: true },
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: true,
+      },
     },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       import: importPlugin,
+      prettier, // <= ADICIONE
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
 
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // ==== Torna divergências do Prettier ERROS ESLint (vai sublinhar) ====
+      'prettier/prettier': [
+        'error',
+        {
+          // espelha seu .prettierrc, para evitar qualquer ambiguidade
+          semi: false,
+          singleQuote: true,
+          trailingComma: 'all',
+          printWidth: 100,
+          // opcional, se quiser explicitar indentação:
+          tabWidth: 2,
+          useTabs: false,
+        },
+      ],
 
-      'no-shadow': ['error', { builtinGlobals: false, hoist: 'all', allow: ['top'] }],
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+      'no-shadow': [
+        'error',
+        {
+          builtinGlobals: false,
+          hoist: 'all',
+          allow: ['top'],
+        },
+      ],
       'id-denylist': ['error', 'sb', 'event'],
       'id-length': [
         'error',
-        { min: 2, properties: 'never', exceptions: ['$', '_', 'i', 'j', 'a', 'b', 'x', 'y'] },
+        {
+          min: 2,
+          properties: 'never',
+          exceptions: ['$', '_', 'i', 'j', 'a', 'b', 'x', 'y'],
+        },
       ],
-      'no-confusing-arrow': ['error', { allowParens: true }],
+      'no-confusing-arrow': [
+        'error',
+        {
+          allowParens: true,
+        },
+      ],
       'no-param-reassign': ['error'],
       'no-magic-numbers': [
         'warn',
-        { ignore: [-1, 0, 1, 2, 10], ignoreArrayIndexes: true, detectObjects: false },
+        {
+          ignore: [-1, 0, 1, 2, 10],
+          ignoreArrayIndexes: true,
+          detectObjects: false,
+        },
       ],
       'new-cap': ['error'],
       'template-curly-spacing': ['error', 'never'],
       'object-shorthand': ['error', 'always'],
       'object-curly-newline': [
         'error',
-        { ObjectExpression: { minProperties: 1 }, ObjectPattern: { multiline: true } },
+        {
+          ObjectExpression: {
+            minProperties: 1,
+          },
+          ObjectPattern: {
+            multiline: true,
+          },
+        },
       ],
       'no-restricted-globals': ['error', 'escape', 'unescape', 'event', 'top'],
       'no-restricted-imports': [
         'error',
-        { name: 'lodash', message: "Please import from 'utils' instead." },
+        {
+          name: 'lodash',
+          message: "Please import from 'utils' instead.",
+        },
       ],
-
       'import/order': [
         'error',
         {
           groups: ['builtin', 'external', 'internal'],
-          pathGroups: [{ pattern: '{react,react-dom/**}', group: 'external', position: 'before' }],
+          pathGroups: [
+            {
+              pattern: '{react,react-dom/**}',
+              group: 'external',
+              position: 'before',
+            },
+          ],
           pathGroupsExcludedImportTypes: ['react'],
           'newlines-between': 'always',
-          alphabetize: { order: 'asc', caseInsensitive: true },
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
         },
       ],
-
       'sort-imports': 'off',
       'no-return-await': 'off',
       'no-useless-return': 'off',
