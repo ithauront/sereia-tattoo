40744bc4667256da1a53f49831e1b9904fee5dd7
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-09-01 17:01:48 +0200

    config[root]: added lint and prettier rules and update ci to run lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,110 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'import': importPlugin,
+      'prettier': prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-shadow': ['error', {
+      builtinGlobals: false,
+      hoist: 'all',
+      allow: ['top']
+    }],
+    'prettier/prettier': 'error',
+    'id-blacklist': [
+      'error',
+      'sb', 'event' // couple of errors happened with that
+    ],
+    'id-length': ['error', {
+      min: 2,
+      properties: 'never',
+      exceptions: [
+        '$', '_',
+        'i', 'j', // loops
+        'a', 'b', // sorting
+        'x', 'y' // dimensions
+      ]
+    }],
+    'no-confusing-arrow': ['error', {
+      allowParens: true
+    }],
+    'no-param-reassign': ['error'],
+    'no-magic-numbers': ['warn', {
+      ignore: [-1, 0, 1, 2, 10], // array operations, parseInt
+      ignoreArrayIndexes: true,
+      detectObjects: false
+    }],
+    'no-return-await': ['off'],
+    'no-useless-return': ['off'],
+    'func-name-matching': ['off'],
+    'new-cap': ['error'],
+    'no-sync': ['off'],
+    'newline-per-chained-call': ['off', {
+      ignoreChainWithDepth: 4
+    }],
+    'template-curly-spacing': ['error', 'never'],
+    'object-shorthand': ['error', 'always'],
+    'object-curly-newline': ['error', {
+      ObjectExpression: {
+        minProperties: 1
+      },
+      ObjectPattern: {
+        multiline: true
+      }
+    }],
+    'prefer-rest-params': ['off'],
+    'valid-jsdoc': ['off'],
+    'no-unused-expressions': ['off'],
+    'no-restricted-globals': ['error', 'escape', 'unescape', 'event', 'top'],
+    'no-restricted-imports': ['error', {
+      'name': 'lodash',
+      'message': 'Please import from \'utils\' instead. '
+    }],
+    'sort-imports': ['off'],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: '{react,react-dom/**}',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'class-methods-use-this': ['off'],
+    },
   },
-])
+)
