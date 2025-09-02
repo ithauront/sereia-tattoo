8494e32d11eb1e717650984ac31db15d03a4210a
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-09-02 10:56:17 +0200

    fix[root/vite.config]: resolve type error

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-/// <reference types="vitest/config" />
-
-// https://vite.dev/config/
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-const dirname =
-  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
-
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
