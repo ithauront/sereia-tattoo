d63aeebabcfc8ea0a89548a54edf7b8dbf8c2324
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2025-10-23 14:31:33 +0200

    Feat[pages/Login]: styling input and logo in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 
 import { useLocation, useNavigate } from 'react-router-dom'
 
+import logo from '../../public/Logo_Sereia.png'
+import { Button } from '../components/Button/Button'
+import TextInput from '../components/TextInput/TextInput'
 import { login } from '../lib/api'
 
 type AuthRedirectState = {
@@ -11,6 +14,7 @@ type AuthRedirectState = {
 export function LoginPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [status, setStatus] = useState('')
   const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
   const navigate = useNavigate()
@@ -24,7 +28,7 @@ export function LoginPage() {
       await login(username, password)
 
       const state = location.state as AuthRedirectState | null
-      const redirectPath = state?.from?.pathname ?? '/admins'
+      const redirectPath = state?.from?.pathname ?? '/admin'
       navigate(redirectPath, {
         replace: true,
       })
@@ -37,31 +41,62 @@ export function LoginPage() {
   }
 
   return (
-    <div className="max-w-sm p-6">
-      <h1 className="mb-4 text-2xl font-bold">Login Page</h1>
-
-      <form onSubmit={handleSubmit} className="space-y-3">
-        <input
-          className="w-full rounded border px-3 py-2"
-          placeholder="username"
-          value={username}
-          onChange={(changeEvent) => setUsername(changeEvent.target.value)}
-        />
-
-        <input
-          className="w-full rounded border px-3 py-2"
-          placeholder="password"
-          type="password"
-          value={password}
-          onChange={(changeEvent) => setPassword(changeEvent.target.value)}
-        />
-
-        {errorMessage && <p className="text-sm text-red-600">{errorMessage}</p>}
-
-        <button className="rounded bg-blue-600 px-4 py-2 text-white" type="submit">
-          Entrar
-        </button>
-      </form>
+    <div className="flex flex-col items-center justify-center ">
+      <div className=" flex items-center justify-center">
+        <div className="w-22 h-auto pl-4 pt-2 ">
+          <img
+            src={logo}
+            alt="Logotipo Sereia Tattoo"
+            role="button"
+            tabIndex={0}
+            aria-label="Voltar para a página inicial"
+          />
+        </div>
+      </div>
+      <div className=" flex items-center justify-center pt-8 w-90 h-70">
+        <form
+          className="flex flex-col gap-4"
+          onSubmit={handleSubmit}
+          aria-label="Formulário de login"
+        >
+          <div className="input-group">
+            <TextInput
+              value={username}
+              onChange={(element) => setUsername(element.target.value)}
+              label="Email"
+            />
+          </div>
+          <div>
+            <TextInput
+              label="Senha"
+              isPassword
+              value={password}
+              onChange={(element) => setPassword(element.target.value)}
+            />
+          </div>
+          <div className="w-full flex item-center justify-center">
+            <Button className="w-30" type="submit" aria-label="Entrar" onClick={() => handleSubmit}>
+              Entrar
+            </Button>
+          </div>
+          {status && (
+            <p
+              role="alert"
+              style={
+                status !== 'Login realizado com sucesso!'
+                  ? {
+                      color: 'white',
+                      backgroundColor: '#25bee473',
+                    }
+                  : {}
+              }
+              aria-live="polite"
+            >
+              {status}
+            </p>
+          )}
+        </form>
+      </div>
     </div>
   )
 }
