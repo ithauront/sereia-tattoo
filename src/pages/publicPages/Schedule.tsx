f7e555282cfe10b304092414f582614447862e54
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2026-03-01 11:42:35 +0100

    feat[pages/pblicpages/schedule]: centrilize text on calendar header

--- src/pages/publicPages/Schedule.tsx
+++ src/pages/publicPages/Schedule.tsx
@@ -1,10 +1,12 @@
+import { Calendar } from '../../components/Calendar/Calendar'
 import { Header } from '../../components/composed/Header/Header'
 
 export function Schedule() {
   return (
     <>
       <Header />
-      <h1 className="text-2xl font-bold">AGENDE CONOSCO</h1>
+      <h1 className="text-center text-slate-900 text-2xl p-4 font-bold">AGENDE CONOSCO</h1>
+      <Calendar />
     </>
   )
 }
