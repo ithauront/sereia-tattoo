ecaaa18d7933453330e8e3b534326162905dbd82
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2026-03-01 11:41:57 +0100

    feat[main]: added import of dayjs and locale pt br on main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import dayjs from 'dayjs'
+
+import 'dayjs/locale/pt-br'
 import { App } from './App'
 import './index.css'
+
+dayjs.locale('pt-br')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
